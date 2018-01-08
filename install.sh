0a3753ff517bc859eca3c6c72f70d7ab6e85090a
h-dr4g0n <33449999+h-dr4g0n@users.noreply.github.com>
2018-01-08 14:34:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-
+#!/bin/bash
+./msfinstall
+sudo apt-get install git
+git config --global user.name "h-dr4g0n"
+git config --global user.email "dragonmatrix2506@gmail.com"
+cat ~/.gitconfig
+#git add *
+#git status
+#git commit -m "loi nhan"
+#git push origin master
+export apktool_version=2.3.1
+sudo -E sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_$apktool_version.jar -O /usr/local/bin/apktool.jar'
+sudo chmod +r /usr/local/bin/apktool.jar
+sudo sh -c 'wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool'
+sudo chmod +x /usr/local/bin/apktool
+sudo apt-get install lib32stdc++6 lib32ncurses5 lib32z1
+Y
+git clone https://gist.github.com/a62d106b55562cc1ab88.git
